e803b83304da154d1e6f4b6e2d3a8437f229a181
Pedro Martins <pedrovrima@gmail.com>
2022-03-18 22:46:20 -0300

    starts function to create check for values

---
+++ components/fields/create_check.js
@@ -0,0 +1,11 @@
+export default function create_check(default_value, unit) {
+  if (capture_categorical_options) {
+// get all values from array and return a function that checks if value is accepted
+} else {
+    if (unit == "perc") {
+        // return funciton that checks if value 0=<val=<100
+    }
+  }
+//   return function that checks if value is numeric 
+  ;
+}
