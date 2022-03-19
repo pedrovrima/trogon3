4919b43376d2e9d575fc0a5c69828e3fa443e457
Pedro Martins <pedrovrima@gmail.com>
2022-03-19 11:57:53 -0300

    adds more elements to the field compoenent

--- components/fields/input.jsx
+++ components/fields/input.jsx
@@ -1,25 +1,45 @@
 import {
-    FormControl,
-    FormLabel,
-    FormErrorMessage,
-    Input
-  } from '@chakra-ui/react'
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  Heading,
+} from "@chakra-ui/react";
 
-
-  
-
-export default function InputField (props){
-    const {form_objects,field,mandatory} = props
-    const {register,error}=form_objects
-    const {name,portuguese_label,duplicable,type,unit,precision,capture_categorical_options}
-    return (
-        <>
-        <FormControl>
-       <FormLabel>{`${portuguese_label}${unit?` (${unit})`:""}`}</FormLabel>
-        {duplicable? "":<Input {...register(name,{required:true}) }></Input>}
-        
-        </FormControl>
-        </>
-    )
-
+import create_check from "./create_check";
+import create_unit from "./create_units"
+export default function InputField(props) {
+  const { form_objects, field, mandatory } = props;
+  const { register, errors } = form_objects;
+  const {
+    name,
+    portuguese_label,
+    duplicable,
+    type,
+    unit,
+    precision,
+    capture_categorical_options,
+  } = field;
+  return (
+    <>
+      <FormControl p={4} isInvalid={errors[name]}>
+        <FormLabel>
+          <Heading size="sm">{`${portuguese_label}${unit ? ` (${create_unit(unit)})` : ""}`}</Heading>
+        </FormLabel>
+        {duplicable ? (
+          ""
+        ) : (
+          <Input
+            {...register(name, {
+              required: "Campo obrigatório",
+              validate: create_check(capture_categorical_options, unit),
+            })}
+          ></Input>
+        )}
+        <FormErrorMessage>
+          {errors[name] && errors[name].message}
+        </FormErrorMessage>{" "}
+      </FormControl>
+    </>
+  );
 }
