dffb024caafd0261cb76435667c89f96fd703131
Pedro Martins <pedrovrima@gmail.com>
2022-03-19 11:46:57 -0300

    changes create_checks to TS to enforce type and finishes it

---
+++ components/fields/create_check.ts
@@ -0,0 +1,16 @@
+export default function create_check(
+  capture_categorical_options: [String],
+  unit: String
+) {
+  if (capture_categorical_options.length) {
+    return (value: String) =>
+      capture_categorical_options.includes(value) || "Valor não permitido";
+  }
+
+  if (unit == "perc") {
+    return (value: Number) =>
+      (value <= 100 && value >= 0) || "Valor deve ser uma porcentagem";
+  }
+
+  return (value) => new Number(value) || "Valor deve ser numérico";
+}
