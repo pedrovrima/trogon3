65a1b13c0180f23b2828911b3bfd1439878fa825
Pedro Martins <pedrovrima@gmail.com>
2022-03-18 22:47:52 -0300

    start capture form

---
+++ components/capture-form/index.js
@@ -0,0 +1,9 @@
+export default function CaptureForm(props) {
+  return (
+    <>
+      {/* Part 1 - MetaData */}
+      {/* Part 2 - Variables */}
+      {/* Part 3 - Age, Sex, Notes and Extra Variables */}
+    </>
+  );
+}
