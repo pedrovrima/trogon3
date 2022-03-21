9a2b553cc2a208a827e76503d1046e783b915e97
Pedro Martins <pedrovrima@gmail.com>
2022-03-21 15:44:29 -0300

    bander api's

--- pages/api/get_bander_id.js
+++ pages/api/get_bander_id.js
@@ -2,12 +2,10 @@
 import prisma from "../../lib/prisma";
 
 export default async function (req, res) {
-  const variables = await prisma.BANDER_REGISTER.findMany({
-    select: {
-      bander_id: true,
-      name: true,
-      code: true,
-    },
+    const id =   (JSON.parse(req.body)).id;
+    console.log(id)
+    const variables = await prisma.BANDER_REGISTER.findUnique({
+      where:{bander_id:id}
   });
   res.send(variables);
 }
