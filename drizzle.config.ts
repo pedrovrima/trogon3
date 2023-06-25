30c5fe022dd3c39acdb3c75b20b18d5d68274129
Pedro Vítor Ribeiro Martins <pedrovrima@gmail.com>
2023-06-25 13:23:55 -0300

    fix typo

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,6 @@ import type { Config } from "drizzle-kit";
 export default {
   schema: "./src/schema.ts",
   out: "./drizzle",
-  connectionString: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${procss.env.DB_HOST}:3306/${process.env.DB_DATABASE}`
+  connectionString: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:3306/${process.env.DB_DATABASE}`
     
 } satisfies Config;
