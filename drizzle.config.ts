18073bdaf043a6dd9b90505ea2b6750840dfd9ba
Pedro Vítor Ribeiro Martins <pedrovrima@gmail.com>
2023-07-01 14:10:59 -0300

    Delete drizzle.config.ts

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "drizzle-kit";
-import {env} from "@/env.mjs"; 
-
-export default {
-  schema: "./src/schema.ts",
-  out: "./drizzle",
-  connectionString: `mysql://${env.DB_USER}:${env.DB_PASSWORD}@${env.DB_HOST}:3306/${env.DB_DATABASE}`
-    
-} satisfies Config;
