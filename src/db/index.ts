890133cfb0956765e68338b5be424968bbcd2de8
Pedro Vítor Ribeiro Martins <pedrovrima@gmail.com>
2023-07-24 23:38:17 -0300

    test - add just locally funcitoning dev db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,16 +1,12 @@
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from "@planetscale/database";
 
-const env = process.env.NODE_ENV || "development";
-
+const env = process.env.NODE_ENV;
+console.log(env);
 const connection = connect({
   host: process.env.DB_HOST,
-  username:
-    env === "development" ? process.env.DB_USER_DEV : process.env.DB_USER_PROD,
-  password:
-    env === "development"
-      ? process.env.DB_PASSWORD_DEV
-      : process.env.DB_PASSWORD_PROD,
+  username: process.env.DB_USER_DEV,
+  password: process.env.DB_PASSWORD_DEV,
 });
 
 const db = drizzle(connection);
