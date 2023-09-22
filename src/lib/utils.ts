71a31a8dbf25e04a9773d76a9604e3a2539df3da
Pedro Vítor Ribeiro Martins <pedrovrima@gmail.com>
2023-09-21 23:18:35 -0300

    remove unused function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,19 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-function convertToCSV(objArray: Object[]): string {
-  let str = `${Object.keys(objArray[0])
-    .map((value) => `"${value}"`)
-    .join(",")}\r\n`;
-
-  for (let i = 0; i < array.length; i++) {
-    let line = "";
-    for (const index in array[i]) {
-      if (line !== "") line += ",";
-      line += `"${array[i][index]}"`;
-    }
-    str += line + "\r\n";
-  }
-  return str;
-}
