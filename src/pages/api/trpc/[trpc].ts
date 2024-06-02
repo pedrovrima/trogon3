2aacc110122f3e04228ab02db41bf1fb72f47e58
Pedro Martins <pedrovrima@gmail.com>
2024-06-01 21:54:00 -0300

    remove next auth again

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,13 +1,13 @@
 import { createNextApiHandler } from "@trpc/server/adapters/next";
 
 import { env } from "@/env.mjs";
-import { createTRPCContext } from "@/server/api/trpc";
+// import { createTRPCContext } from "@/server/api/trpc";
 import { appRouter } from "@/server/api/root";
 
 // export API handler
 export default createNextApiHandler({
   router: appRouter,
-  createContext: createTRPCContext,
+  // createContext: createTRPCContext,
   onError: ({ path, error }) => {
     console.log("to aqui");
     console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
